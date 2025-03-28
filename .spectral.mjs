3d33d72feecf5dc4446f706f0eb4740cd4365b75
mairtin.conneely@smartbear.com <mairtin.conneely@smartbear.com>
2025-03-28 10:46:23 +0000

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3Njk4NA/spectral.js?branch_slug=main&token=2126880c-1a6a-4989-8fd0-b4ab870a6431";
+export default { extends: ruleset };
